a5fd5bf62e4a8fa4ced17bffde051c997b88a03c
Iisak Haukkala <iisak.haukkala@gmail.com>
2022-11-22 14:59:00 +0200

    toimiva testi

--- src/tests/app_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestRegister(unittest.TestCase):
-    def setUp(self):
-        print("Set up goes here")
-
-    def test_hello_world(self):
-        self.assertEqual("Hello world", "Hello world")
